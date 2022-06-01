dbb6c1d0a8ed566e377e67ca9f06c5b860be59b4
JAllen43 <60601777+JAllen43@users.noreply.github.com>
2022-06-01 14:05:41 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'menusystem',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
